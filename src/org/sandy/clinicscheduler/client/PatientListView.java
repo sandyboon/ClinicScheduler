385fec82792350696e27c2f5fd7d1e73e06e05b8
Sandarbh Singh <sandy.boon@gmail.com>
2023-09-19 04:55:06 -0400

    added click handlers

--- src/org/sandy/clinicscheduler/client/PatientListView.java
+++ src/org/sandy/clinicscheduler/client/PatientListView.java
@@ -1,12 +1,18 @@
 package org.sandy.clinicscheduler.client;
 
+import org.sandy.clinicscheduler.shared.IDGenerator;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.SpanElement;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -35,8 +41,29 @@ public class PatientListView extends Composite {
 		patientTable.setText(0, 0, "Patient ID");
 		patientTable.setText(0, 1, "Patient Name");
 		patientTable.setText(0, 2, "Patient Schedule");
+		initClickHandlers();
 	}
 	
+
+	private void initClickHandlers() {		
+		addPatientButton.addClickHandler(ce -> this.addPatientRow());
+	}
+
+
+	
+	private void addPatientRow() {
+		int rowIndexToInsert = patientTable.getRowCount() ;
+		patientTable.insertRow(rowIndexToInsert);
+		patientTable.setText(rowIndexToInsert, 0, String.valueOf(IDGenerator.PATIENT_ID_GENERATOR.getPatientId()));
+		patientTable.setText(rowIndexToInsert, 1, patientNameTextBox.getValue());
+//		patientTable.setText(rowIndexToInsert, 2, "Book Appointment");
+		// add click handler for the appointment cell
+		Anchor bookAppointmentLink = new Anchor("Book Appointment");
+		bookAppointmentLink.addClickHandler(ce -> Window.alert("I am clicked"));
+		
+		 patientTable.setWidget(rowIndexToInsert, 2, bookAppointmentLink );
+	}
+
 
 	// Add a setter method for the doctorNameLabel
 	public void setDoctorName(String name) {
